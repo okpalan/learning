41f454810f78863c5019dd4f8bb9db0e8efe0341
Nnamdi Michael Okpala <okpalan86@gmail.com>
2022-01-16 02:05:16 +0000

    a

---
+++ chapter-01/excercises/arthematic.lua
@@ -0,0 +1,22 @@
+print(13 * 15)
+print(type(13 * 15))
+
+print(-3 * 15)
+print(13.0 + 24)
+print(3 / 2)
+
+--[[ 
+floor division    
+--]]
+print(3 // 2)
+print(3.0 // 2)
+print(6.0 // 2.0)
+print(-9 // 2)
+print(1.5 // 0.5)
+
+-- modulo operator
+-- a % b == a -(( a // b ) * b)
+
+x = math.pi
+x = x%0.01
+
---
+++ chapter-01/excercises/numbers.lua
@@ -0,0 +1,11 @@
+print("type(3)",type(3))
+print("type(3.5)",type(3.5))
+print("type(3.0)",type(3.0))
+
+print(1 == 1.0)
+print(-3 == -3.0)
+print(0.2e3 == 2000)
+
+
+print(math.type(3))
+print(math.type(3.0))
