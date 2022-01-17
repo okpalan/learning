384d05b27a948aa4b14997a96e66b006ca38f46c
Nnamdi Michael Okpala <okpalan86@gmail.com>
2022-01-17 11:04:15 +0000

    a

---
+++ learning-lua/chapter-03/excercise.lua
@@ -0,0 +1,19 @@
+-- print(".0e12",.0e12)
+-- print(".e12",.e12)
+-- print("0.0e",0.0e)
+
+print("0x12",0x12)
+
+-- print("0xABFG",0xABFG)
+print("0xA",0xA)
+
+-- print("FFFF",FFFF)
+print("0xFFFFFFF",0xFFFFFFF) --[[valid in lua--]]
+-- print("0x",0x)
+-- 1p flaoting part not double represented but half a double
+-- print(0x1P10) 
+print(0.1e1) 
+
+-- print(0x0.1p1)
+
+print(math.maxinterger * 2 )
--- learning-lua/chapter-03/string.lua
+++ learning-lua/chapter-03/string.lua
@@ -1,11 +1,15 @@
 x = (2 ^ 52) + 1
-print(string.format("%d %d",x, math.floor(x + 0.5)))
+print(string.format("%d %d", x, math.floor(x + 0.5)))
 
+-- incorrect 
 function round(x)
-    local f =math.floor(x)
+    local f = math.floor(x)
     if (x == f) or (x % 2.0 == 0.5) then
         return f
     else
         return math.floor(x+0.5)
     end
 end
+
+print(round(2.5))
+print(round(-2.5))
