4e2a6a15402603ebfe71377f992644d65497ac80
Nnamdi Michael Okpala <okpalan86@gmail.com>
2022-01-29 17:05:17 +0000

    a

---
+++ learning-c/chapter13/read_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int read_line(char str[],int n){
+ int ch,i=0;
+    while ((ch=getchar()) != '\n')
+        if(i < n)
+        str[i++]=ch;
+    str[i]='\0';
+    return i;
+}
+
+int count_spaces(const char *s){
+    int count = 0 , i;
+
+    for(; *s !='\0'; i++)
+        if(*s == ' ')
+        count++;
+    return count;
+}
+
+int main(void){
+}
+
